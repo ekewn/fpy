e77f4ac50beaaa662a7a4ac489b90561bb8ea257
Ellis Coon <ellis.coon@equusoft.com>
2024-04-14 14:54:19 -0600

    wrapped up

--- tools.py
+++ tools.py
@@ -1,3 +1,4 @@
+from operator import add
 from typing import Any, Callable, Optional
 
 #
@@ -8,7 +9,7 @@ from typing import Any, Callable, Optional
 # Used to indicate any operation that might raise an exception.
 type Failable[T] = T | Exception
 
-# Used to indicate code that has a side effect. Implicitly failable.
+# Used to indicate code that has a side effect.
 type SideEffect = Failable[None]
 
 #
@@ -17,6 +18,7 @@ type SideEffect = Failable[None]
 #
 #
 is_exception: Callable[[Any], bool] = lambda x: isinstance(x, Exception)
+is_none: Callable[[Any], bool] = lambda x: x is None
 
 
 def asserts(expression: Any) -> None:
@@ -26,22 +28,39 @@ def asserts(expression: Any) -> None:
     assert expression
 
 
-def log(f: Callable) -> Callable:
+def log[T, U](f: Callable[[T], U]) -> Callable[[T], U]:
     """
     Prints the name, input, and output of a function.
     """
 
-    def _(*args, **kwargs):
+    def _(arg):
         print(f"Function: {f.__name__}")
-        print(f"Input: {args}")
-        result = f(*args, **kwargs)
+        print(f"Input: {arg}")
+        result = f(arg)
         print(f"Output: {result}")
         return result
 
     return _
 
 
-def opt_map[T, U](f: Callable[[T], U]) -> Callable[[Optional[T]], Optional[U]]:
+def map_opt[T](v: T) -> Optional[T]:
+    """
+    Maps a value to an optional value.
+    """
+    v if not is_none(v) else None
+
+
+def map_fail[T](v: T) -> Failable[T]:
+    """
+    Maps a value to a failable value.
+    """
+    if not is_exception(v):
+        return v
+    else:
+        return Exception(v)
+
+
+def bind_opt[T, U](f: Callable[[T], U]) -> Callable[[Optional[T]], Optional[U]]:
     """
     Wraps the inputs and outputs of a function in an Optional tag so that continuation
     can be used. If an arg is None, None is returned so that they can continue to persist
@@ -50,10 +69,10 @@ def opt_map[T, U](f: Callable[[T], U]) -> Callable[[Optional[T]], Optional[U]]:
     return lambda x: f(x) if x is not None else x
 
 
-def fail_map[T, U](f: Callable[[T], U]) -> Callable[[Failable[T]], Failable[U]]:
+def bind_fail[T, U](f: Callable[[T], U]) -> Callable[[Failable[T]], Failable[U]]:
     """
     Wraps the inputs and outputs of a function in an Failable tag so that continuation
-    can be used. If an arg is an Exception, it is returned is returned so that they can continue to persist
+    can be used. If an arg is an Exception, it is returned so that they can continue to persist
     throughout a pipeline.
     """
     return lambda x: f(x) if not is_exception(x) else x  # type: ignore
@@ -66,18 +85,12 @@ def fail_map[T, U](f: Callable[[T], U]) -> Callable[[Failable[T]], Failable[U]]:
 #
 if __name__ == "__main__":
 
-    def add_one(x: int) -> int:
-        return x + 1
+    add_one: Callable[[int], int] = lambda x: x + 1
+    tuplify: Callable[[int], tuple[int]] = lambda x: (x,)
+    listify: Callable[[int], list[int]] = lambda x: [x]
 
-    def strang(x: int) -> str:
-        return str(x)
+    add_one_wlogging = log(add_one)
 
-    log(opt_map(add_one))(1)
-    log(opt_map(add_one))(None)
-    log(opt_map(strang))(2)
-    log(opt_map(strang))(None)
-    log(fail_map(add_one))(3)
-    log(fail_map(add_one))(Exception("this isn't great"))
-    log(fail_map(strang))(4)
-    log(fail_map(strang))(Exception("asdfasd"))
+    add_one_wlogging(1)
+    bind_opt(add_one_wlogging)(None)
 
